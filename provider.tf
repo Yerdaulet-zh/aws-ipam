9737084234e4331aee7d57cf496271bbdf506a14
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-11 21:23:53 +0500

    add multiple providers based on three regions

--- provider.tf
+++ provider.tf
@@ -13,3 +13,15 @@ provider "aws" {
   region  = "eu-central-1"
   profile = "895587011312_AdministratorAccess"
 }
+
+provider "aws" {
+  alias   = "us_east_1"
+  region  = "us-east-1"
+  profile = "895587011312_AdministratorAccess"
+}
+
+provider "aws" {
+  alias   = "ap_south_1"
+  region  = "ap-south-1"
+  profile = "895587011312_AdministratorAccess"
+}
