c54044ed9811d1f3b2d07be4da91577734d18c47
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-14 19:53:37 +0500

    used netmask length to let AWS provide Globally Unique Address (GUA) for private networking

--- environment/root/ipv6/aws_vpc_ipam_pool_cidr.tf
+++ environment/root/ipv6/aws_vpc_ipam_pool_cidr.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc_ipam_pool_cidr" "ipv6_root" {
   ipam_pool_id = aws_vpc_ipam_pool.ipv6_root.id
-  cidr         = "fd00:abcd:1234::/48"
+  # cidr         = "fd00:abcd:1234::/48"
+  netmask_length = 48
 
   depends_on = [aws_vpc_ipam_pool.ipv6_root]
 }
