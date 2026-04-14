3ad6d44dc7c4479cbc636f47f56499bf9adba1c1
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-14 12:01:37 +0500

    remove african provider

--- environment/provider.tf
+++ environment/provider.tf
@@ -19,9 +19,3 @@ provider "aws" {
   region  = "us-east-1"
   profile = "895587011312_AdministratorAccess"
 }
-
-provider "aws" {
-  alias   = "ap_south_1"
-  region  = "ap-south-1"
-  profile = "895587011312_AdministratorAccess"
-}
